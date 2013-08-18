a3f158e087e30f9b7aad7c9af77744c7e6a2e55b
Inquisitor-sasha <inquisitorsasha@sturmkrieg.de>
2013-08-18 01:45:12 -0400

    Updating extension to be functional

--- WikiBanner.php
+++ WikiBanner.php
@@ -2,18 +2,25 @@
 
 $wgExtensionCredits[$type][] = array(
         'path' => __FILE__,
-        'name' => "",
-        'description' => "",
-        'descriptionmsg' => "",
-        'version' => 0,
-        'author' => "",
-        'url' => "",
+        'name' => "WikiBanner",
+        'description' => "Allows top and bottom banners to be added to the wiki as configured in the LocalSettings.php file.",
+//      'descriptionmsg' => "",
+        'version' => 1.1,
+        'author' => "Inquisitor Ehrenstein",
+        'url' => "http://www.mediawiki.org/wiki/Extension:WikiBanner",
 );
+
+
+//Explicitly defining global variables
+
+$wgTopBannerCode = '<!-- No Top Banner -->';
+$wgBottomBannerCode = '<!-- No Bottom Banner -->';
+
+//Code for adding the top and bottom banners to the wiki
 
 $wgHooks['BeforePageDisplay'][] = 'WikiBanner';
 function WikiBanner( OutputPage &$out, Skin &$skin ) {
-	global $wgTopBannerCode = '<!-- No Top Banner -->';
-	global $wgBottomBannerCode = '<!-- No Bottom Banner -->';
+	global $wgTopBannerCode, $wgBottomBannerCode;
 
 	$out->prependHTML( $wgTopBannerCode );
 	$out->addHTML( $wgBottomBannerCode );
